d16cf6abab89d36c940fa94500ea6ee81191821d
Hong-Jupjup <hshh0113@gmail.com>
2024-02-05 14:21:51 +0900

    15654

---
+++ 15654.py
@@ -0,0 +1,12 @@
+# 24.02.05
+
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+array = list(map(int, input().split()))
+array.sort()
+
+for i in permutations(array, M):
+    print(*i)
